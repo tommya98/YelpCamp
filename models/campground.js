4036e16641a93c787c322b95b587322077b0636a
tommya98 <tommya98@naver.com>
2023-09-27 17:58:40 +0900

    FEAT: 리뷰 delete 미들웨어 구현

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import Review from './review.js';
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -8,6 +9,16 @@ const CampgroundSchema = new Schema({
   description: String,
   location: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 export default mongoose.model('Campground', CampgroundSchema);
