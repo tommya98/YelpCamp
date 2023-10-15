3e82360c2e5486a05647fe6f73904a9fd399c030
tommya98 <tommya98@naver.com>
2023-10-15 13:22:46 +0900

    FEAT: 이미지 썸네일 url 추가

--- models/campground.js
+++ models/campground.js
@@ -3,9 +3,18 @@ import Review from './review.js';
 
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
