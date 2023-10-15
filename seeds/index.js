22c7fbe4fa8e26984bf58c0bd01c96bf4908667e
tommya98 <tommya98@naver.com>
2023-10-15 12:34:01 +0900

    FEAT: DB 수정

--- seeds/index.js
+++ seeds/index.js
@@ -25,10 +25,19 @@ const seedDB = async () => {
       author: `65237aa6b27624183840f884`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe mollitia impedit corporis delectus eaque iste neque dolorum vel tempore quos ut itaque quasi nobis, asperiores placeat ducimus ratione cum earum!',
       price,
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dtm3cksar/image/upload/v1697340628/YelpCamp/utbatgzqyhvhq44majwi.jpg',
+          filename: 'YelpCamp/utbatgzqyhvhq44majwi',
+        },
+        {
+          url: 'https://res.cloudinary.com/dtm3cksar/image/upload/v1697340630/YelpCamp/iwpbckf0ubryivfqnzb2.jpg',
+          filename: 'YelpCamp/iwpbckf0ubryivfqnzb2',
+        },
+      ],
     });
     await camp.save();
   }
