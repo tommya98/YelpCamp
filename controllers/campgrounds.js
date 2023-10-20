2fd55b82fd860173cf2736ace34a69c1d6fdce32
tommya98 <tommya98@naver.com>
2023-10-20 13:43:50 +0900

    FEAT: Mapbox 지도 연동

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,9 @@
 import Campground from '../models/campground.js';
+import mbxGeocoding from '@mapbox/mapbox-sdk/services/geocoding.js';
 import { cloudinary } from '../cloudinary/index.js';
+
+const mapBoxToekn = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToekn });
 
 export const index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,7 +15,14 @@ export const renderNewForm = (req, res) => {
 };
 
 export const createCampground = async (req, res, next) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.author = req.user._id;
   await campground.save();
