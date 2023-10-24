773c797cbe0bbdd2941b61b85a2d2a76794ebaa9
tommya98 <tommya98@naver.com>
2023-10-24 16:19:45 +0900

    FEAT: helmet 적용

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ import methodOverride from 'method-override';
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import mongoSanitize from 'express-mongo-sanitize';
+import helmet, { contentSecurityPolicy } from 'helmet';
 
 import ExpressError from './utils/ExpressError.js';
 import userRoutes from './routes/users.js';
@@ -61,6 +62,47 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+  'https://stackpath.bootstrapcdn.com/',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/',
+  'https://api.tiles.mapbox.com/',
+  'https://api.mapbox.com/',
+  'https://kit.fontawesome.com/',
+  'https://cdnjs.cloudflare.com/',
+  'https://cdn.jsdelivr.net',
+];
+const styleSrcUrls = [
+  'https://kit-free.fontawesome.com/',
+  'https://stackpath.bootstrapcdn.com/',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/',
+  'https://api.mapbox.com/',
+  'https://api.tiles.mapbox.com/',
+  'https://fonts.googleapis.com/',
+  'https://use.fontawesome.com/',
+];
+const connectSrcUrls = [
+  'https://api.mapbox.com/',
+  'https://a.tiles.mapbox.com/',
+  'https://b.tiles.mapbox.com/',
+  'https://events.mapbox.com/',
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https://res.cloudinary.com/dtm3cksar/', 'https://images.unsplash.com/'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  }),
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
